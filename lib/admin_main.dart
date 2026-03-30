1e2a7aa8804b17fae38b7c9dabe9c88b78f06ea2
yadukrishnan <yadukrish9656@gmail.com>
2026-03-30 16:22:28 +0530

    initial flutter project setup

---
+++ lib/admin_main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CustomerApp());
+}
+
+class CustomerApp extends StatelessWidget {
+  const CustomerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text("Admin App"))),
+    );
+  }
+}
